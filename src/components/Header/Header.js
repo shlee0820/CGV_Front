51140453fe3c5825e6cd349bfe933e60fa8fe80d
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-06-10 03:11:59 +0900

    헤더 부분 버튼누르면 창 생성 완료

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 
 import logo from '../../assets/cgv_logo.png';
@@ -11,12 +11,48 @@ import hambergericon from '../../assets/Hamburger_icon.png';
 import './Header.css';
 
 const Header = () => {
+
+    const [ isVisible, setIsVisible ] = useState(false);
+
+    const onClickImg = () => {
+        setIsVisible(true);
+    }
+
+    const onClickClose = () => {
+        setIsVisible(false);
+    }
+
     return (
         <header className='header-box'>
             <Link to='/'>
                 <img id='logo' style={{width: "100%", height: "90%", margin: "2%"}} src={logo}/>
             </Link>
-            <img id='hidden-btn' src={hambergericon}/>
+            <img id='hidden-btn' src={hambergericon} onClick={onClickImg}/>
+            { isVisible ? 
+                <section id='total-box'>
+                    <div className='row'>
+                        <div className='h-btn-box'>
+                            <Link to='/login'><img className='h-btn-img' src={login}/></Link>
+                            <span>로그인</span>
+                        </div>
+                        <div className='h-btn-box'>
+                            <Link to='/join'><img className='h-btn-img' src={join}/></Link>
+                            <span>회원가입</span>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='h-btn-box'>
+                            <Link to='/my'><img className='h-btn-img' src={mycgv}/></Link>
+                            <span>MY CGV</span>
+                        </div>
+                        <div className='h-btn-box'>
+                            <Link to='/#'><img className='h-btn-img' src={center}/></Link>
+                            <span>고객센터</span>
+                        </div>
+                    </div>
+                    <button onClick={onClickClose}>X</button>
+                </section>
+            : null }
             <section className='btn-group-box'>
                 <div className='btn-box'>
                     <Link to='/login'><img className='btn-img' src={login}/></Link>
