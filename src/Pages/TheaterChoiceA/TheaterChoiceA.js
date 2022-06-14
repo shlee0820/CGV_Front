8d19c359a8b428072afeb9e28e53ceeaf66cc8e3
JaeHyunGround <gothddlek@naver.com>
2022-06-14 21:36:48 +0900

    극장 2페이지 구현 완료

--- src/Pages/TheaterChoiceA/TheaterChoiceA.js
+++ src/Pages/TheaterChoiceA/TheaterChoiceA.js
@@ -10,10 +10,21 @@ import JoinHeader from '../../components/JoinHeader/JoinHeader';
 function TheaterChoiceA() {
     const [amTimeMovie, pmTimeMovie] = [1, 2];
     const [choice, setChoice] = useState(false);
+    const [movieAname, setMovieAName] = useState();
+    const [movieAPoster, setMovieAPoster] = useState();
+
+    const [movieAShowData, setMovieAShowData] = useState('');
 
     function timeclick(event) {
         setChoice(event.target.id);
-
+        if (choice == amTimeMovie) {
+            setMovieAShowData('62a59fdb9e6ddf6621cf2d4b');
+        } else {
+            setMovieAShowData('62a5ccab1a58e38fbe6f8b2c');
+        }
+        console.log(movieAShowData)
+        // 만약 오전 11시 영화를 선택했다면 movieAShowData에 오전 11시 브로커 영화의 정보가 들어가고 18시 영화를 선택했다면 18시
+        // 브로커 영화의 정보가 들어간다.
     }
 
     const [loginStatus, setLoginStatus] = useState(false);
@@ -23,7 +34,6 @@ function TheaterChoiceA() {
             .get("/get_login_id")
             .then(res => res.data)
             .then(res => {
-                console.log(res);
                 if (res.islogin === true) {
                     setLoginStatus(true);
 
@@ -33,8 +43,27 @@ function TheaterChoiceA() {
             })
     }
 
+    function AmovieData() {
+        axios
+            .get('/movie')
+            .then(res => res.data.movies)
+            .then(res => res[1].contents)
+            .then(res => {
+                setMovieAName(res.name);
+                setMovieAPoster(res.poster);
+            });
+    }
+
+    const test = () => {
+        axios
+            .get('/theater')
+            .then(res => console.log(res))
+    }
+
     useEffect(() => {
         userTest();
+        AmovieData();
+        test();
     }, []);
 
     return (
@@ -105,48 +134,54 @@ function TheaterChoiceA() {
                 <div className='bottomBox'>
                     <div className='bottomleft'>
                         <div className='poster'>
-                            <p>이미지</p>
+                            <img src={movieAPoster} className='postersize'></img>
                             <div className='posterinfo'>
-                                <p>제목</p>
+                                <p>{movieAname}</p>
                                 <p>2D</p>
-                                <p>(상영 등급)관람가</p>
+                                <p>12세 이상 관람가</p>
                             </div>
                         </div>
                         <div className='bottominfo'>
-                            <p>
+                            <div>
                                 <span className='light'>극장&nbsp;&nbsp;&nbsp;</span>
                                 <span className='bold'>CGV 구미 &gt;</span>
-                            </p>
-                            <p>
+                            </div>
+                            <div>
                                 <span className='light'>일시&nbsp;&nbsp;&nbsp;</span>
-                                <span className='bold'>2022.6.15(수) (영화 시간 받아오기)</span>
-                            </p>
-                            <p>
+                                <span className='bold'>2022.6.15(수)</span>
+                            </div>
+                            <div>
                                 <span className='light'>상영관&nbsp;&nbsp;&nbsp;</span>
-                                <span className='bold'>(상영관 정보)</span>
-                            </p>
-                            <p>
+                                <span className='bold'>1관 1층</span>
+                            </div>
+                            <div>
                                 <span className='light'>인원&nbsp;&nbsp;&nbsp;</span>
-                            </p>
+                            </div>
                         </div>
-                        <div className='step'></div>
+                        <div className='step'>
+                            <p className='steptext'>좌석선택 → 결제</p>
+                        </div>
                     </div>
                     <div className='bottomright'>
-                        <Link
-                            to={(
-                                choice)
-                                ? '/theater/seat'
-                                : '/theater/choiceMovieA'}
-                            className='bottombtn'>
-                            <div
-                                className={(
-                                    choice)
-                                    ? 'active_btn'
-                                    : 'btn'}>
-                                <p>➜</p>
-                                <p>좌석 선택</p>
-                            </div>
-                        </Link>
+                        <div className='rightbtn'>
+                            {/* <Link to={(choice) ? '/theater/seat' : '/theater/choiceMovieA'}> */}
+                            <Link
+                                to={{
+                                    pathname: (choice) ? '/theater/seat' : '/theater/choiceMovieA',
+                                    state: { 
+                                        idData : {movieAShowData}
+                                    }
+                                }}>
+                                <div
+                                    className={(
+                                        choice)
+                                        ? 'active_btn'
+                                        : 'btn'}>
+                                    <p>➜</p>
+                                    <p>좌석 선택</p>
+                                </div>
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
